dcaa0d361836d50cbf30cda862778e0a1d317c02
Dawa192 <Dawa192@Dawa>
2015-02-05 12:18:32 +0545

    sql added

---
+++ Mountains/seq.sql
@@ -0,0 +1,34 @@
+/*
+SQLyog Ultimate v9.50 
+MySQL - 5.6.17 : Database - mountain
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`mountain` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `mountain`;
+
+/*Table structure for table `mountain` */
+
+DROP TABLE IF EXISTS `mountain`;
+
+CREATE TABLE `mountain` (
+  `id` varchar(20) NOT NULL,
+  `Mname` varchar(20) NOT NULL,
+  `Altitude` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `mountain` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
